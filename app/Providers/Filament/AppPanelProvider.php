fb74b0bfda8da39ff395da97a2cc98aa09bef870
ahmetkocabiyik <ahmetkocabiyik@gmail.com>
2025-09-09 16:46:01 +0300

    sidebar collapsable hale getirildi

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -58,6 +58,7 @@ class AppPanelProvider extends PanelProvider
                 Authenticate::class,
             ])
             ->sidebarCollapsibleOnDesktop()
+            ->sidebarFullyCollapsibleOnDesktop()
             ->readOnlyRelationManagersOnResourceViewPagesByDefault(false);
     }
 }
