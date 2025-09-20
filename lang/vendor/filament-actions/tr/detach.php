535ca3138329c92afe25e4f3acf9d1e64ab970d7
ahmetkocabiyik <ahmetkocabiyik@gmail.com>
2025-09-20 21:34:46 +0300

    Ayır texti Sil ile güncellendi

---
+++ lang/vendor/filament-actions/tr/detach.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+
+    'single' => [
+
+        'label' => 'Sil',
+
+        'modal' => [
+
+            'heading' => ':label sil',
+
+            'actions' => [
+
+                'detach' => [
+                    'label' => 'Sil',
+                ],
+
+            ],
+
+        ],
+
+        'notifications' => [
+
+            'detached' => [
+                'title' => 'Silindi',
+            ],
+
+        ],
+
+    ],
+
+    'multiple' => [
+
+        'label' => 'Seçiliyi sil',
+
+        'modal' => [
+
+            'heading' => ':label seçiliyi sil ',
+
+            'actions' => [
+
+                'detach' => [
+                    'label' => 'Seçiliyi sil',
+                ],
+
+            ],
+
+        ],
+
+        'notifications' => [
+
+            'detached' => [
+                'title' => 'Silindi',
+            ],
+
+        ],
+
+    ],
+
+];
